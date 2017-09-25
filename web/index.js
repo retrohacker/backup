a9bffd340d33e59577a4caac9bd808375192fb9f
retrohacker <retrohacker@protonmail.com>
2017-09-24 23:44:15 -0700

    loading animation

--- web/index.js
+++ web/index.js
@@ -2,17 +2,35 @@ const choo = require('choo')
 const html = require('choo/html')
 const app = choo()
 
+var state = 0
+var states = [
+  '/setup'
+]
+
 function setupCard(content) {
   return html`
     <body>
       <div class="absolute w-100 h-100 setup-splash-bg"></div>
       <div class="absolute w-75 h-75 br4 bg-white o-80 center"></div>
-      <div class="absolute maxw-75 maxh-75 scroll center tc">
+      <div class="absolute maxw-75 maxh-75 center tc">
         ${content}
       </div>
     </body>
   `
 }
+
+function onNext(state, emit) {
+  emit.pushState('loading')
+  request('/state', function(e, res, body) {
+  });
+  return setupCard(html`
+    <div><h1 class="f1 gray"><i class="fa spin fa-circle-o-notch"></i></h1></div>
+  `);
+}
+
+app.route('/loading', function(state, emit) {
+
+});
 
 app.route('/setup', function () {
   return setupCard(html`
@@ -26,7 +44,7 @@ app.route('/setup', function () {
 })
 
 app.route('*', function (state, emit) {
-  emit('pushState', '/setup')
+  return onNext();
 })
 
 app.mount('body')
